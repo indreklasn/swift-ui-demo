c2b67306ac1941dc9e02854dc851056268229125
Indrek Lasn <lasnindrek@gmail.com>
2019-12-27 15:10:34 +0200

    Initial Commit

---
+++ fetchexample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fetchexample
+//
+//  Created by Indrek Lasn on 27.12.19.
+//  Copyright © 2019 Indrek Lasn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
