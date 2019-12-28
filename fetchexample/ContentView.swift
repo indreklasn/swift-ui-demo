1140228df7c951e9b7104c5ea37b3aea35e3637e
Indrek Lasn <lasnindrek@gmail.com>
2019-12-28 15:29:47 +0200

    init

--- fetchexample/ContentView.swift
+++ fetchexample/ContentView.swift
@@ -8,14 +8,36 @@
 
 import SwiftUI
 
+func greet(name: String) -> String {
+	return "Greetings from \(name)"
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
+	var body: some View {
+		VStack {
+			Text(greet(name: "Indrek Lasn"))
+				.font(.largeTitle)
+				.fontWeight(.ultraLight)
+				.padding()
+
+			VStack {
+				HStack {
+					Image("cat")
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+					Image("cat")
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+				}
+			}
+
+		}
+
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
--- fetchexampleTests/fetchexampleTests.swift
+++ fetchexampleTests/fetchexampleTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import fetchexample
 
-class fetchexampleTests: XCTestCase {
+class FetchexampleTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
